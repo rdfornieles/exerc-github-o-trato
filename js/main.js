22711ee32e6f5b5c5182a501319fd46aa277f10e
rdfornieles <rdfornieles@gmail.com>
2018-12-16 12:47:26 +0100

    name split fix

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const inputEl = document.querySelector(".nickName");
 
 const btnEl = document.querySelector(".btn");
 
-const container = document.querySelector(".container");
+const listEl = document.querySelector(".container");
 
 
 function userName(event) {
@@ -14,15 +14,22 @@ function userName(event) {
     let inputValue = inputEl.value;
     fetch(`https://api.github.com/users/${inputValue}`)
     .then(function(response) {
-        console.log('response',response);
+       
         return response.json();
     })
     .then(function(data) {
-        console.log(data);
+        const fullName = data.name.split("");
+        const justName = fullName[0];
         console.log(data.name);
-        
-        
-})
-}
+        console.log(fullName);
+        console.log(justName);
+
+        let name = '';
+        for (let i = 0; i < justName.length; i++) {
+            name += `<ol><li>${justName[i]}</li></ol>`;
+        }
+        listEl.innerHTML = name;
+});
+};
 
 btnEl.addEventListener('click', userName);
